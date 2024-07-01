be6a4dacd6b4edd78a3f2be6ea44802953b8ff62
Sedat Budak <sbudak@cyh.com.tr>
2024-07-01 11:07:38 +0300

    removed scaffold from MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Scaffold(),
     );
   }
 }
