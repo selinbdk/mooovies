5ec60be403956084bb0a150a5a48de31950146a6
Sedat Budak <sbudak@cyh.com.tr>
2024-07-03 10:29:27 +0300

    fixed- favorite view page

---
+++ lib/src/views/favorite_view.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:mooovies/src/core/theme/app_colors.dart';
+
+class FavoriteView extends StatefulWidget {
+  const FavoriteView({super.key});
+
+  @override
+  State<FavoriteView> createState() => _FavoriteViewState();
+}
+
+class _FavoriteViewState extends State<FavoriteView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backColor,
+      appBar: AppBar(
+        backgroundColor: AppColors.topbar,
+        title: const Center(
+          child: Text(
+            "YOUR FAVORITE MOVIES",
+            style: TextStyle(
+              color: AppColors.text,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          "THERE ARE NO MOVIES IN THE LIST",
+          style: TextStyle(
+            color: AppColors.text,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
