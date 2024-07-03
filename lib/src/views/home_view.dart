6cc670954795c0ba57089128d44529e51e74fb22
Sedat Budak <sbudak@cyh.com.tr>
2024-07-03 11:07:09 +0300

    added- carried grid_scrolling part into homeview page

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -3,7 +3,10 @@
 //* fu-stf
 //*
 import 'package:flutter/material.dart';
-import 'package:mooovies/src/core/components/cards/movie_card.dart';
+//import 'package:mooovies/src/core/components/cards/movie_card.dart';
+import 'package:mooovies/src/core/theme/app_colors.dart';
+import 'package:mooovies/src/views/_widgets/home_search_field.dart';
+//import 'package:mooovies/src/views/favorite_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -11,7 +14,102 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MyWidget(),
+      backgroundColor: AppColors.backColor,
+      appBar: AppBar(
+        elevation: 0,
+        surfaceTintColor: Colors.transparent,
+        backgroundColor: AppColors.topbar,
+        title: const Row(
+          children: [
+            Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            SizedBox(width: 18),
+            //* textformfield
+            //* textfield
+            Expanded(child: HomeTextField()),
+          ],
+        ),
+      ),
+      body: Column(children: [
+        Container(
+          color: AppColors.secondaryColor,
+          width: 800,
+          height: 50,
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 4, vertical: 6),
+            child: Text(
+              "POPULAR MOVIES",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: AppColors.text,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: GridView.builder(
+            scrollDirection: Axis.horizontal,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              crossAxisSpacing: 1,
+              mainAxisSpacing: 1,
+            ),
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('design/harry-potter.webp'),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        Container(
+          color: AppColors.secondaryColor,
+          width: 800,
+          height: 50,
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 4, vertical: 6),
+            child: Text(
+              "NOW PLAYING",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: AppColors.text,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: GridView.builder(
+            scrollDirection: Axis.horizontal,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              crossAxisSpacing: 1,
+              mainAxisSpacing: 1,
+            ),
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('design/harry-potter.webp'),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ]),
+
+// Column => text , gridview, text gridview
     );
   }
 }
