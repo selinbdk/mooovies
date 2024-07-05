b3d1440c7d502fb46f9324d7da9396ff4020ebed
Sedat Budak <sbudak@cyh.com.tr>
2024-07-05 14:53:17 +0300

    feat: created MovieRepository adn added getNowPlayingMovies function

--- lib/src/core/repository/repository.dart
+++ lib/src/core/repository/repository.dart
@@ -1,7 +1,14 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:mooovies/src/core/constants/app_constants.dart';
+import 'package:mooovies/src/core/models/movie_listings_model.dart';
+
 //**
-// Burada API'ye gidip istek atılacak.  
-// Bize bir JSON veri dönecek. 
-// Dönen JSON veriyi parse edip dart modellerine çevireceğiz. 
+// Burada API'ye gidip istek atılacak.
+// Bize bir JSON veri dönecek.
+// Dönen JSON veriyi parse edip dart modellerine çevireceğiz.
 // Daha sonrasında bu modelleri UI sayfalarında göstereceğiz.
 //
 //
@@ -9,6 +16,26 @@
 //
 //
 //
-//? API URL: https://api.themoviedb.org/3 
+//? API URL: https://api.themoviedb.org/3
+//? API URL: https://api.themoviedb.org/3/popular-movies
 //? API TOKEN: eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZmFkZDliNzY5NTU2ZjQ1NWRjYmE2NGZlYzU2NmYxOCIsInN1YiI6IjY0MWQ5NGYyMzQ0YThlMDBiYTNjMDBiNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nofxbbCpeqzAJ_XCXwgvzKZrO_KQqgYIErKx8T-HvLc
 // */
+//* how can i convert my json data to dart classes
+class MovieRepository {
+  Future<MovieListingsModel> getNowPlayingMovies() async {
+    final response = await http.get(
+      Uri.parse(AppConstants.baseUrl + AppConstants.nowPlayingMoviePath),
+      headers: {
+        HttpHeaders.contentTypeHeader: ContentType.json.mimeType,
+        HttpHeaders.authorizationHeader: 'Bearer ${AppConstants.tmdbToken}',
+      },
+    );
+
+    final data = jsonDecode(response.body);
+
+    final movieListingsModel =
+        MovieListingsModel.fromJson(data as Map<String, dynamic>);
+
+    return movieListingsModel;
+  }
+}
