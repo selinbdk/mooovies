e4d8e700eca940cb431a09e4ec16cf14c187b8d1
Sedat Budak <sbudak@cyh.com.tr>
2024-07-08 16:01:36 +0300

    feat- added BaseNetworkImage

--- lib/src/core/components/cards/movie_card.dart
+++ lib/src/core/components/cards/movie_card.dart
@@ -1,6 +1,10 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:mooovies/src/core/components/button/favorite_button.dart';
+import 'package:mooovies/src/core/components/image/base_network_image.dart';
+import 'package:mooovies/src/core/constants/app_constants.dart';
+import 'package:mooovies/src/core/models/movie_model.dart';
 import 'package:mooovies/src/core/theme/app_colors.dart';
 
 //import 'package:mooovies/src/views/navigation_bar.dart';
@@ -25,19 +29,21 @@ const Image(
 */
 
 class MovieCard extends StatelessWidget {
-  const MovieCard({super.key});
+  const MovieCard({super.key, required this.movie});
+
+  final MovieModel? movie;
 
   @override
   Widget build(BuildContext context) {
     //* burası sadece movie görsel ve ismini içerecek
     //* Container kullanılsın
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(16),
       child: Stack(
         children: <Widget>[
-          Image.asset(
-            'design/harry-potter.webp',
-            fit: BoxFit.contain,
+          BaseNetworkImage(
+            url: AppConstants.originalImage(movie?.posterPath) ?? '',
           ),
           Positioned.fill(
             child: Column(
@@ -45,49 +51,71 @@ class MovieCard extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 const Padding(
-                  padding: EdgeInsets.all(12),
-                  child: Icon(
-                    Icons.favorite,
-                    color: Colors.red,
-                    size: 40,
-                  ),
+                  padding: EdgeInsets.all(1),
+                  child: FavoriteButton(),
                 ),
                 ClipRRect(
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
-                    child: const SizedBox(
-                      height: 60,
-                      child: Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              "Harry Potter",
-                              style: TextStyle(
-                                color: AppColors.text2,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18,
-                              ),
-                            ),
-                            Row(
-                              children: [
-                                Text(
-                                  "7.3",
-                                  style: TextStyle(
-                                    color: AppColors.text2,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18,
+                  child: Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.black,
+                          Colors.black45,
+                          Colors.transparent,
+                        ],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                      ),
+                    ),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+                      child: SizedBox(
+                        height: 70,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: SingleChildScrollView(
+                                  scrollDirection: Axis.vertical,
+                                  child: Text(
+                                    (movie?.title ?? ''),
+                                    //* burası sığmıyor
+                                    // maxLines: 1,
+                                    style: const TextStyle(
+                                      color: AppColors.text2,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
                                   ),
                                 ),
-                                Icon(
-                                  Icons.star,
-                                  color: Colors.amber,
-                                  size: 18,
-                                ),
-                              ],
-                            ),
-                          ],
+                              ),
+                              const SizedBox(width: 10),
+                              Row(
+                                //mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    movie?.voteAverage!.toStringAsFixed(1) ??
+                                        '',
+                                    //* 2 haneliye çevirlemli
+
+                                    style: const TextStyle(
+                                      color: AppColors.text2,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  const Icon(
+                                    Icons.star,
+                                    color: Colors.amber,
+                                    size: 18,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
